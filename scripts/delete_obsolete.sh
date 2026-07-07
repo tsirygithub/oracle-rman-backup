4a23a2c3531de55906e678d33b360f8943cb82f7
tsirygithub <84145448+tsirygithub@users.noreply.github.com>
2026-07-07 15:08:07 +0300

    Add script to delete obsolete Oracle backups

---
+++ scripts/delete_obsolete.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ORACLE_HOME=/u01/app/oracle/product/19.0.0/dbhome_1
+ORACLE_SID=ORCL
+
+export ORACLE_HOME
+export ORACLE_SID
+export PATH=$ORACLE_HOME/bin:$PATH
+
+rman target / <<EOF
+
+CROSSCHECK BACKUP;
+
+DELETE EXPIRED BACKUP;
+
+DELETE NOPROMPT OBSOLETE;
+
+EXIT;
+
+EOF
